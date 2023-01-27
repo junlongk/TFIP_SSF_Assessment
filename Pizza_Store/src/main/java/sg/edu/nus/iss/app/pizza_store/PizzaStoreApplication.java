a1dba5f63948a6038da1fe6f7dcc4193960b5fa8
Jun Long <keejunlong@hotmail.com>
2023-01-27 09:45:06 +0800

    Completed Task 1

---
+++ Pizza_Store/src/main/java/sg/edu/nus/iss/app/pizza_store/PizzaStoreApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.pizza_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaStoreApplication.class, args);
+    }
+
+}
---
+++ Pizza_Store/src/test/java/sg/edu/nus/iss/app/pizza_store/PizzaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.pizza_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
